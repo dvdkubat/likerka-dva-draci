abe537e98f45fca26944f39fbbc246cfc499e21f
dvdkubat <dvd.kubat@gmail.com>
2026-03-10 07:48:12 +0100

    Add files via upload

---
+++ reviews.js
@@ -0,0 +1,20 @@
+
+const reviewNames = ["Honza K.", "Petra M.", "Radek B.", "Ivana Š.", "Miroslav D.", "Jana V.", "Tomáš N.", "Lucie F.", "Pavel Č.", "Markéta H."];
+
+const reviewTexts = [
+  "Po dvou panácích jsem zapomněl proč jsem přišel, ale byl to báječný večer.",
+  "Chuť je silná. Paměť už méně. Znovu objednávám.",
+  "Objednal jsem jednu láhev, probudil jsem se se třemi. Přidávám pátou hvězdu.",
+  "Po třetím panáku je všechno jedno. Opravdu všechno.",
+  "5/5 – doporučuji i nepřátelům. Hlavně nepřátelům.",
+  "Ráno jsem našel prázdnou láhev a nové přátele, které neznám.",
+  "Nejdřív pálí, pak nepálí nic. Přesně jak má být.",
+  "Dodáno rychle, zásilka přišla celá. Bohužel.",
+  "Můj lékař říkal abych nekupoval. Koupil jsem dvě láhve.",
+  "Překvapivě chutné, pokud si zvyknete na pachu.",
+  "Ideální dárek pro tchyni. Nebo ex-přítele.",
+  "Otevřeli jsme na Silvestra a skončilo to hasičským zásahem.",
+  "Zkoušel jsem dražší alternativy. Tohle je jiné. Jiné v tom nejlepším smyslu.",
+  "Zákazník roku 2024 a 2025. Pracuji na roku 2026.",
+  "Nedávejte to dětem. To je vše co k tomu řeknu.",
+];
